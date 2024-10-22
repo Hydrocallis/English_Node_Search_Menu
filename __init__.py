b129e490f9da6d1b29f7cc5a83edb5e4e3e90793
Hydrocallis <makoto.turu@gmail.com>
2024-10-22 18:40:48 +0900

    v1_0_0-beta

---
+++ __init__.py
@@ -0,0 +1,47 @@
+bl_info = {
+    "name": "English Node Search Menu",
+    "author": "KSYN",
+    "version": (1, 0, 0),
+    "blender": (4, 2, 0),
+    "location": "Node Editor > Add Menu",
+    "description": "Adds English search functionality to the node editor add menu",
+    "warning": "",
+    "category": "Node"
+}
+
+import bpy
+
+class NODE_OT_search_en_add_menu(bpy.types.Operator):
+    bl_idname = "node.search_en_add_menu"
+    bl_label = "Search Add Menu (English)"
+    bl_description = "Open the add menu search in English"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        # メニューを直接呼び出す
+        bpy.ops.wm.search_single_menu("INVOKE_DEFAULT", menu_idname="NODE_MT_add")
+        # 言語を元に戻す
+        context.preferences.view.language = self.current_lang
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        # 現在の言語設定を保存
+        self.current_lang = context.preferences.view.language
+        # UI言語を英語に設定
+        context.preferences.view.language = 'en_US'
+        return self.execute(context)
+
+def add_search_operator(self, context):
+    self.layout.separator()
+    self.layout.operator(NODE_OT_search_en_add_menu.bl_idname)
+
+def register():
+    bpy.utils.register_class(NODE_OT_search_en_add_menu)
+    bpy.types.NODE_MT_add.append(add_search_operator)
+
+def unregister():
+    bpy.types.NODE_MT_add.remove(add_search_operator)
+    bpy.utils.unregister_class(NODE_OT_search_en_add_menu)
+
+if __name__ == "__main__":
+    register()
